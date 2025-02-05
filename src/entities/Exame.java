be8030f9c65d7b91eb9da0774c88f278e0c07724
Pedro <pedrovieira1355@gmail.com>
2025-02-05 20:02:00 -0300

    criacao das classes medicamento, exame e prescricao e associacao das duas primeiras com uma prescricao

---
+++ src/entities/Exame.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Date;
+
+public class Exame {
+    private int idExame;
+    private String tipoDoExame;
+    private Date dataDePrescricao;
+    private Date dataDeRealizacao;
+    private String resultadoDoExame;
+    private Double precoDoExame;
+
+    public Exame(String tipoDoExame, Date dataDePrescricao, Date dataDeRealizacao, String resultadoDoExame, Double precoDoExame) {
+        this.tipoDoExame = tipoDoExame;
+        this.dataDePrescricao = dataDePrescricao;
+        this.dataDeRealizacao = dataDeRealizacao;
+        this.resultadoDoExame = resultadoDoExame;
+        this.precoDoExame = precoDoExame;
+    }
+
+    public String getTipoDoExame() {
+        return tipoDoExame;
+    }
+
+    public void setTipoDoExame(String tipoDoExame) {
+        this.tipoDoExame = tipoDoExame;
+    }
+
+    public Date getDataDePrescricao() {
+        return dataDePrescricao;
+    }
+
+    public void setDataDePrescricao(Date dataDePrescricao) {
+        this.dataDePrescricao = dataDePrescricao;
+    }
+
+    public Date getDataDeRealizacao() {
+        return dataDeRealizacao;
+    }
+
+    public void setDataDeRealizacao(Date dataDeRealizacao) {
+        this.dataDeRealizacao = dataDeRealizacao;
+    }
+
+    public String getResultadoDoExame() {
+        return resultadoDoExame;
+    }
+
+    public void setResultadoDoExame(String resultadoDoExame) {
+        this.resultadoDoExame = resultadoDoExame;
+    }
+
+    public Double getPrecoDoExame() {
+        return precoDoExame;
+    }
+
+    public void setPrecoDoExame(Double precoDoExame) {
+        this.precoDoExame = precoDoExame;
+    }
+}
---
+++ src/entities/Prescricao.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Prescricao {
+    private int idPrescricao;
+    private Consulta consulta;
+    private ArrayList<Medicamento> medicamentosPrescritos;
+    private ArrayList<Exame> examesPrescritos;
+
+    public Prescricao(int id, Consulta consulta) {
+        this.idPrescricao = id;
+        this.consulta = consulta;
+        this.medicamentosPrescritos = new ArrayList<>();
+        this.examesPrescritos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return idPrescricao;
+    }
+
+    public void setId(int id) {
+        this.idPrescricao = id;
+    }
+
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+
+    public ArrayList<Medicamento> getMedicamentosPrescritos() {
+        return medicamentosPrescritos;
+    }
+
+    public void setMedicamentosPrescritos(ArrayList<Medicamento> medicamentosPrescritos) {
+        this.medicamentosPrescritos = medicamentosPrescritos;
+    }
+
+    public ArrayList<Exame> getExamesPrescritos() {
+        return examesPrescritos;
+    }
+
+    public void setExamesPrescritos(ArrayList<Exame> examesPrescritos) {
+        this.examesPrescritos = examesPrescritos;
+    }
+}
