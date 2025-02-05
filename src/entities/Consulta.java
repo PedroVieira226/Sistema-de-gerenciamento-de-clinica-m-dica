cabd39e728d13793fab73c192a10db792e1e0436
Pedro <pedrovieira1355@gmail.com>
2025-02-05 19:49:08 -0300

    criacao dos atributos da classe consulta

--- src/entities/Consulta.java
+++ src/entities/Consulta.java
@@ -1,5 +1,18 @@
 package entities;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Consulta {
     private int idConsulta;
+    private Date dataDaConsulta;
+    private LocalDateTime horarioDeInicio;
+    private LocalDateTime duracaoDaConsulta;
+    private Paciente pacienteDaConsulta;//associação de classe com a classe paiente, relação "tem um"
+    private Medico medicoDaConsulta;
+    private Double valorConsulta;
+
+
+
 }
