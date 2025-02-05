22dbe8996a823dad95025172e55f2acf5775528c
Pedro <pedrovieira1355@gmail.com>
2025-02-05 19:42:29 -0300

    criacao de ids para cada entidade criada e crm e especialidade do medico adicionados

--- src/entities/Medico.java
+++ src/entities/Medico.java
@@ -3,10 +3,15 @@ package entities;
 import java.util.Date;
 
 public class Medico extends PessoaFisica{
+    private Integer idMedico;
+    private String crm;
+    private String especialidade;
 
-    public Medico(String nome, String cpf, Date dataDeNascimento) {
+
+    public Medico(String nome, String cpf, Date dataDeNascimento, String crm, String especialidade, Integer idMedico) {
         super(nome, cpf, dataDeNascimento);
+        this.crm = crm;
+        this.especialidade = especialidade;
+        this.idMedico = idMedico;
     }
-
-
 }
--- src/entities/Paciente.java
+++ src/entities/Paciente.java
@@ -3,10 +3,12 @@ package entities;
 import java.util.Date;
 
 public class Paciente extends PessoaFisica{
+    private Integer idPaciente;
 
 
-    public Paciente(String nome, String cpf, Date dataDeNascimento) {
+    public Paciente(String nome, String cpf, Date dataDeNascimento, Integer idPaciente) {
         super(nome, cpf, dataDeNascimento);
+        this.idPaciente = idPaciente;
     }
 
 
