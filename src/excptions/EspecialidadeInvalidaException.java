747909ab45e250aa3bb870dfdf93a1240664d9d0
Pedro <pedrovieira1355@gmail.com>
2025-02-05 21:40:11 -0300

    criacao das excessoes personalizadas

---
+++ src/excptions/EspecialidadeInvalidaException.java
@@ -0,0 +1,7 @@
+package excptions;
+
+public class EspecialidadeInvalidaException extends RuntimeException {
+    public EspecialidadeInvalidaException(String message) {
+        super(message);
+    }
+}
---
+++ src/excptions/HorarioIndisponivelException.java
@@ -0,0 +1,7 @@
+package excptions;
+
+public class HorarioIndisponivelException extends RuntimeException {
+    public HorarioIndisponivelException(String message) {
+        super(message);
+    }
+}
