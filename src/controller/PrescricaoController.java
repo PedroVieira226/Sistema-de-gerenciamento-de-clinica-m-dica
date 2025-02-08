87d9d21e51d4f8d9605cb2d48b5acfc85ff80394
kdukoelho <ce.figueiredoc@gmail.com>
2025-02-08 11:17:51 -0300

    feat: prescricao controller

---
+++ src/controller/PrescricaoController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import entities.Consulta;
+import entities.Exame;
+import entities.Medicamento;
+import entities.Prescricao;
+import repository.PrescricaoRepository;
+
+import java.util.ArrayList;
+
+public class PrescricaoController {
+    private PrescricaoRepository repository;
+    private MedicamentoController medicamentoController;
+    private ExameController exameController;
+
+    PrescricaoController(){
+        repository = new PrescricaoRepository();
+    }
+
+    public Prescricao create(ArrayList<Medicamento> medicamentosPrescritos, ArrayList<Exame> examesPrescritos){
+        Prescricao prescricao = new Prescricao(medicamentosPrescritos, examesPrescritos);
+        repository.createPrescricao(prescricao);
+        return prescricao;
+    }
+
+    public Prescricao createFromIds(ArrayList<Integer> medicamentosPrescritosIds, ArrayList<Integer> examesPrescritosIds){
+        ArrayList<Medicamento> medicamentosPrescritos = new ArrayList<>();
+        for (Integer id : medicamentosPrescritosIds){
+            if(medicamentoController.read(id) != null){
+                medicamentosPrescritos.add(medicamentoController.read(id));
+            }
+        }
+
+        ArrayList<Exame> examesPrescritos = new ArrayList<>();
+        for (Integer id : examesPrescritosIds){
+            if(exameController.read(id) != null){
+                examesPrescritos.add(exameController.read(id));
+            }
+        }
+
+        return create(medicamentosPrescritos, examesPrescritos);
+    }
+
+    public Prescricao read(Integer id){
+        return repository.readPrescricao(id);
+    }
+
+    public void update(Prescricao prescricao){
+        repository.updatePrescricao(prescricao);
+    }
+
+    public void delete(Prescricao prescricao){
+        repository.deletePrescricao(prescricao);
+    }
+
+    public void delete(Integer id){
+        repository.deletePrescricao(repository.readPrescricao(id));
+    }
+}
